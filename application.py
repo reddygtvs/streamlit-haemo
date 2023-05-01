c6534c4e7305a3ae8574fb16d11dfa0767833f22
Tushar <reddygtvs@gmail.com>
2023-05-01 14:11:20 +0530

    added st.camera instaed of webrtc

--- application.py
+++ application.py
@@ -5,8 +5,6 @@ from PIL import Image
 import numpy as np
 import io
 import cv2
-import av
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 # Load the models
 model = load_model("models/binaryClassifierV2.h5")
@@ -35,16 +33,6 @@ def predict(image):
         prediction_label = "Blood sample is not infected"
     return prediction_label
 
-# Define a video transformer to capture the image from the camera
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        # Convert the frame to a PIL Image
-        img = frame.to_image()
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img = Image.fromarray(img)
-
-        return img
-
 # Set the page configuration and title
 st.set_page_config(page_title="Blood Sample Classifier")
 st.title("Blood Sample Classifier")
@@ -52,10 +40,10 @@ st.title("Blood Sample Classifier")
 # Add a button to upload the image
 uploaded_file = st.file_uploader("Choose a blood sample image", type="jpg")
 
-# Add a button to start the webcam
-webrtc_ctx = webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+# Add a button to start the camera
+camera = st.camera_input()
 
-# Get the image from the uploaded file or the webcam and predict
+# Get the image from the uploaded file or the camera and predict
 if uploaded_file is not None:
     image = Image.open(io.BytesIO(uploaded_file.read()))
     st.image(image, caption="Blood sample", use_column_width=True)
@@ -63,8 +51,8 @@ if uploaded_file is not None:
     if st.button("Predict from uploaded file"):
         prediction = predict(image)
         st.header(f"Prediction: {prediction}")
-elif webrtc_ctx.video_transformer:
-    if st.button("Predict from webcam"):
-        image = webrtc_ctx.video_transformer.get_image()
+elif camera is not None:
+    if st.button("Predict from camera"):
+        image = Image.fromarray(camera, "RGB")
         prediction = predict(image)
         st.header(f"Prediction: {prediction}")
